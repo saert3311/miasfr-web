dfb4ab2a69f9599580e5ae8b9ad3b464d0254a5d
Jose Duarte <saert@outlook.com>
2023-06-01 23:25:54 -0400

    remade headings

--- index-services.php
+++ index-services.php
@@ -1,94 +1,94 @@
     <section class="ftco-section ftco-project" data-section="services" id="the-services">
     	<div class="container px-md-5">
     		<div class="row justify-content-center pb-5">
-          <div class="col-md-12 heading-section text-center ftco-animate">
-          	<span class="subheading"><?php echo ___('WHAT WE RENT?') ?></span>
-            <h2 class="mb-4"><?php echo ___('Our Services') ?></h2>
-            <p><?php echo ___('We maintain a full line of scaffold equipment and accessories to meet all your access needs. Our extensive inventory includes:') ?></p>
-          </div>
-        </div>
+    			<div class="col-md-12 heading-section text-center ftco-animate">
+    				<span class="subheading"><?php echo ___('WHAT WE RENT?') ?></span>
+    				<h2 class="mb-4"><?php echo ___('Our Services') ?></h2>
+    				<p><?php echo ___('We maintain a full line of scaffold equipment and accessories to meet all your access needs. Our extensive inventory includes:') ?></p>
+    			</div>
+    		</div>
     		<div class="row">
     			<div class="col-md-12 testimonial">
-            <div class="carousel-project owl-carousel">
-			<div class="item">
-            		<div class="project">
-		    					<div class="img">
-								<a href="system-scaffold"><img src="https://miamiscaffoldrental.com/images/msr_system_scaffold.jpg" class="img-fluid" alt="Scaffold MIASCFR"></a>
-			    				</div>
-			    				<div class="text px-4">
-			    					<h2><a href="system-scaffold"><?php echo ___('System Scaffold') ?></a></h2>
-			    					<span><?php echo ___('Quick and easy') ?></span>
-			    				</div>
-		    				</div>
-            	</div>
-            	<div class="item">
-            		<div class="project">
-		    					<div class="img">
-								<a href="frame-scaffolding"><img src="https://miamiscaffoldrental.com/images/msr_scaffold.jpg" class="img-fluid" alt="Scaffold MIASCFR"></a>
-			    				</div>
-			    				<div class="text px-4">
-			    					<h2><a href="frame-scaffolding"><?php echo ___('Frame Scaffolding') ?></a></h2>
-			    					<span><?php echo ___('All your access needs') ?></span>
-			    				</div>
-		    				</div>
-            	</div>
-            	<div class="item">
-            		<div class="project">
-		    					<div id="masalto" class="img">
-								<a href="baker-scaffolding"><img src="https://miamiscaffoldrental.com/images/msr_baker.jpg" class="img-fluid" alt="Baker MIASCFR"></a>
-			    				</div>
-			    				<div class="text px-4">
-			    					<h2><a href="baker-scaffolding"><?php echo ___('Baker Scaffolding') ?></a></h2>
-			    					<span><?php echo ___('Safety and ease of use') ?></span>
-			    				</div>
-		    				</div>
-            	</div>
-            	<div class="item">
-            		<div class="project">
-		    					<div class="img">
-								<a href="suspended-scaffold"><img src="https://miamiscaffoldrental.com/images/suspended.png" class="img-fluid" alt="Suspended MIASCFR"></a>
-			    				</div>
-			    				<div class="text px-4">
-			    					<h2><a href="suspended-scaffold"><?php echo ___('Suspended Scaffold') ?></a></h2>
-			    					<span><?php echo ___('High-rise exterior access') ?></span>
-			    				</div>
-		    				</div>
-            	</div>
-            	<div class="item">
-            		<div class="project">
-		    					<div class="img">
-								<a href="post-shoring"><img src="https://miamiscaffoldrental.com/images/shoring.png" class="img-fluid" alt="Shoring MIASCFR"></a>
-			    				</div>
-			    				<div class="text px-4">
-			    					<h2><a href="post-shoring"><?php echo ___('Shoring') ?></a></h2>
-			    					<span><?php echo ___('Support structures') ?></span>
-			    				</div>
-		    				</div>
-            	</div>
-            	<div class="item">
-            		<div class="project">
-		    					<div class="img">
-								<a href="scissor-lift"><img src="https://miamiscaffoldrental.com/images/msr_scissor.jpg" class="img-fluid" alt="Scissor MIASCFR"></a>
-			    				</div>
-			    				<div class="text px-4">
-			    					<h2><a href="scissor-lift"><?php echo ___('Scissor Lift') ?></a></h2>
-			    					<span><?php echo ___('Overhead access') ?></span>
-			    				</div>
-		    				</div>
-            	</div>
-            	<div class="item">
-            		<div class="project">
-		    					<div class="img">
-								<a href="boom-lift"><img src="https://miamiscaffoldrental.com/images/msr_boom.jpg" class="img-fluid" alt="Boom MIASCFR"></a>
-			    				</div>
-			    				<div class="text px-4">
-			    					<h2><a href="boom-lift"><?php echo ___('Boom Lift') ?></a></h2>
-			    					<span><?php echo ___('Up-and-over positioning') ?></span>
-			    				</div>
-		    				</div>
-            	</div>
-            </div>
-          </div>
+    				<div class="carousel-project owl-carousel">
+    					<article class="item">
+    						<div class="project">
+    							<div class="img">
+    								<a href="system-scaffold"><img src="https://miamiscaffoldrental.com/images/msr_system_scaffold.jpg" class="img-fluid" alt="Scaffold MIASCFR"></a>
+    							</div>
+    							<div class="text px-4">
+    								<p><a href="system-scaffold"><?php echo ___('System Scaffold') ?></a></p>
+    								<span><?php echo ___('Quick and easy') ?></span>
+    							</div>
+    						</div>
+						</article>
+    					<article class="item">
+    						<div class="project">
+    							<div class="img">
+    								<a href="frame-scaffolding"><img src="https://miamiscaffoldrental.com/images/msr_scaffold.jpg" class="img-fluid" alt="Scaffold MIASCFR"></a>
+    							</div>
+    							<div class="text px-4">
+    								<p><a href="frame-scaffolding"><?php echo ___('Frame Scaffolding') ?></a></p>
+    								<span><?php echo ___('All your access needs') ?></span>
+    							</div>
+    						</div>
+						</article>
+    					<article class="item">
+    						<div class="project">
+    							<div id="masalto" class="img">
+    								<a href="baker-scaffolding"><img src="https://miamiscaffoldrental.com/images/msr_baker.jpg" class="img-fluid" alt="Baker MIASCFR"></a>
+    							</div>
+    							<div class="text px-4">
+    								<p><a href="baker-scaffolding"><?php echo ___('Baker Scaffolding') ?></a></p>
+    								<span><?php echo ___('Safety and ease of use') ?></span>
+    							</div>
+    						</div>
+						</article>
+    					<article class="item">
+    						<div class="project">
+    							<div class="img">
+    								<a href="suspended-scaffold"><img src="https://miamiscaffoldrental.com/images/suspended.png" class="img-fluid" alt="Suspended MIASCFR"></a>
+    							</div>
+    							<div class="text px-4">
+    								<p><a href="suspended-scaffold"><?php echo ___('Suspended Scaffold') ?></a></p>
+    								<span><?php echo ___('High-rise exterior access') ?></span>
+    							</div>
+    						</div>
+						</article>
+    					<article class="item">
+    						<div class="project">
+    							<div class="img">
+    								<a href="post-shoring"><img src="https://miamiscaffoldrental.com/images/shoring.png" class="img-fluid" alt="Shoring MIASCFR"></a>
+    							</div>
+    							<div class="text px-4">
+    								<p><a href="post-shoring"><?php echo ___('Shoring') ?></a></p>
+    								<span><?php echo ___('Support structures') ?></span>
+    							</div>
+    						</div>
+						</article>
+    					<article class="item">
+    						<div class="project">
+    							<div class="img">
+    								<a href="scissor-lift"><img src="https://miamiscaffoldrental.com/images/msr_scissor.jpg" class="img-fluid" alt="Scissor MIASCFR"></a>
+    							</div>
+    							<div class="text px-4">
+    								<p><a href="scissor-lift"><?php echo ___('Scissor Lift') ?></a></p>
+    								<span><?php echo ___('Overhead access') ?></span>
+    							</div>
+    						</div>
+						</article>
+    					<article class="item">
+    						<div class="project">
+    							<div class="img">
+    								<a href="boom-lift"><img src="https://miamiscaffoldrental.com/images/msr_boom.jpg" class="img-fluid" alt="Boom MIASCFR"></a>
+    							</div>
+    							<div class="text px-4">
+    								<p><a href="boom-lift"><?php echo ___('Boom Lift') ?></a></p>
+    								<span><?php echo ___('Up-and-over positioning') ?></span>
+    							</div>
+    						</div>
+						</article>
+    				</div>
+    			</div>
     		</div>
     	</div>
     </section>
