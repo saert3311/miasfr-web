3a9bc194872b3ad7792e19b823b4646578f1dcda
Jose Duarte <saert@outlook.com>
2023-07-02 01:34:20 -0400

    Button repairs

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
         </picture>
       </a>
       <button class="navbar-toggler js-fh5co-nav-toggle fh5co-nav-toggle" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="oi oi-menu"></span>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
       </button>
 
       <div class="collapse navbar-collapse" id="ftco-nav">
@@ -181,7 +181,9 @@
       }
     }
   </script>
-  <div class="mobile-call"><a href="tel:+13057901410"><button type="button" class="btn btn-primary"><span class="icon icon-phone"></span></button></a></div>
+  <div class="mobile-call"><a href="tel:+13057901410"><button type="button" class="btn btn-primary">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone-call" style="margin-right: 5px;"><path d="M15.05 5A5 5 0 0 1 19 8.95M15.05 1A9 9 0 0 1 23 8.94m-1 7.98v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>
+  </button></a></div>
   <div class="logo-mobile"><img src="images/msr_logo_bw.webp" alt="Miami Scaffolding Rental Service"></div>
 </body>
 
