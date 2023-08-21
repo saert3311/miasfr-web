7744147191bb8c971fa7cd3834afe7eb462442df
Jose Duarte <saert@outlook.com>
2023-08-20 21:26:35 -0400

    reverse PT

--- common_header.php
+++ common_header.php
@@ -1,43 +1,26 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-  <link rel="manifest" href="/site.webmanifest">
-  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#64aae0">
-  <meta name="msapplication-TileColor" content="#64aae0">
-  <meta name="theme-color" content="#ffffff">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-  <?php echo '<link rel="stylesheet" href="css/msr.css?' . filemtime('css/msr.css') . '" />'; ?>
-  <link rel="preload" href="css/animate.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
-  <link rel="preload" href="css/owl.carousel.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
-  <link rel="preload" href="css/owl.theme.default.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
-  <link rel="stylesheet" href="css/owl.theme.default.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
-  <link rel="stylesheet" href="css/cookiealert.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
-  <script defer src="js/modernizr.js" type="text/javascript"></script>
-  <script src="~partytown/partytown.js" type="text/javascript"></script>
-  <script>
-  	partytown = {
-  		forward: ['dataLayer.push'],
-  	};
-  </script>
-  <!-- Google Tag Manager -->
-  <script type="text/partytown">
-  	(function(w, d, s, l, i) {
-  		w[l] = w[l] || [];
-  		w[l].push({
-  			'gtm.start': new Date().getTime(),
-  			event: 'gtm.js'
-  		});
-  		var f = d.getElementsByTagName(s)[0],
-  			j = d.createElement(s),
-  			dl = l != 'dataLayer' ? '&l=' + l : '';
-  		j.async = true;
-  		j.src =
-  			'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-  		f.parentNode.insertBefore(j, f);
-  	})(window, document, 'script', 'dataLayer', 'GTM-WKR586Z');
-  </script>
-  <!-- End Google Tag Manager -->
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#64aae0">
+	<meta name="msapplication-TileColor" content="#64aae0">
+	<meta name="theme-color" content="#ffffff">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<?php echo '<link rel="stylesheet" href="css/msr.css?' . filemtime('css/msr.css') . '" />'; ?>
+	<link rel="preload" href="css/animate.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+	<link rel="preload" href="css/owl.carousel.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+	<link rel="preload" href="css/owl.theme.default.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+	<link rel="stylesheet" href="css/cookiealert.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+	<script defer  src="js/modernizr.js" type="text/javascript"></script>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WKR586Z');</script>
+<!-- End Google Tag Manager -->
